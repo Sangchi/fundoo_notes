7190a457efb87eed8c18ffcb45108e873978fbaf
Apple <apple@Apples-MacBook-Pro.local>
2024-09-12 18:27:54 +0530

    [prashant chavan]add:label app urls added in settings url file

--- server/fundoo_notes/fundoo_notes/urls.py
+++ server/fundoo_notes/fundoo_notes/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/",include('user.urls')),
-    path('note/', include('note.urls'))
+    path('note/', include('note.urls')),
+    path('label/',include('label.urls'))
 ]
