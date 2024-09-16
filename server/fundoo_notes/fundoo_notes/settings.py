e33243cfd1860ab1b9b7150b3d1232f697e59a43
Apple <apple@Apples-MacBook-Pro.local>
2024-09-16 17:08:38 +0530

    [prashant chavan]add:configuered the redis database in settings

--- server/fundoo_notes/fundoo_notes/settings.py
+++ server/fundoo_notes/fundoo_notes/settings.py
@@ -194,3 +194,15 @@ LOGURU_SETTINGS = {
 # Apply the Loguru settings
 for handler in LOGURU_SETTINGS["handlers"]:
     logger.add(**handler)
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
