508f7698c9068d70cc6a5fcdfaee78cae52b52ec
Apple <apple@Apples-MacBook-Pro.local>
2024-09-18 12:44:47 +0530

    [prashant chavan]add:celery task to send reminder email reminder to user

---
+++ server/fundoo_notes/note/tasks.py
@@ -0,0 +1,27 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+from loguru import logger
+from .models import Notes
+
+
+
+@shared_task
+def send_reminder_email(note_name,email):
+    """
+        A Celery task to send a plain text reminder email to the user.
+
+    """
+    try:
+        
+        body=f"Reminder for Note: {note_name}"
+        subject = f"Reminder"
+        send_mail(
+        subject=subject,
+        message=body,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        fail_silently=False,
+    )
+    except Exception as e:
+        logger.info("Note not found")
