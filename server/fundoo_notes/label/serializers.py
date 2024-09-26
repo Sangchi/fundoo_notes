2cae72080843d7472a1582fd18eb2b85f2c620a1
Apple <apple@Apples-MacBook-Pro.local>
2024-09-26 23:28:26 +0530

    [prashant chavan]refactor:added condtion for title in serializer title should be the string

--- server/fundoo_notes/label/serializers.py
+++ server/fundoo_notes/label/serializers.py
@@ -8,4 +8,9 @@ class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model=Label
         fields='__all__'
-       
+    
+
+    def validate_name(self, value):
+        if isinstance(value, int):
+            raise serializers.ValidationError("The name field cannot be an integer.")
+        return value
