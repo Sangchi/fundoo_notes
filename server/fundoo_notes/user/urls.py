84a0216b02f14ec45380bc98d91af85b7b66146d
Apple <apple@Apples-MacBook-Pro.local>
2024-09-26 23:32:34 +0530

    [prashant chavan]refactor:modifeld the paths of user app urls

--- server/fundoo_notes/user/urls.py
+++ server/fundoo_notes/user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import RegisterUserView, LoginUserView ,verify_registered_user
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('verify/<str:token>',verify_registered_user,name='verify')
+    path('verify/<token>',verify_registered_user,name='verify')
 ]
