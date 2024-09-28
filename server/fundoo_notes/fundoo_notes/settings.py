e3695d94c5dabae4d0cd4ab7b7deee837494262c
Apple <apple@Apples-MacBook-Pro.local>
2024-09-28 12:28:33 +0530

    [prashant chavan]add:configured templates in settings

--- server/fundoo_notes/fundoo_notes/settings.py
+++ server/fundoo_notes/fundoo_notes/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'fundoo_notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
